1b4958f048d6d4cd048e2357ff2715e1e3c4711b
Aaron Gil Rubinos <142986554+AaronRubinos@users.noreply.github.com>
2024-04-10 23:28:07 +0800

    Update output1.sh

--- output1.sh
+++ output1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #Output 1
+
 firstName=Aaron
 lastName=Rubinos
 program=BSCS
+
 lvl=3rd
 yr=yr
+
+echo Hi! I am $firstName $lastName
+echo a $program student,
+echo currently on my $lvl $yr
